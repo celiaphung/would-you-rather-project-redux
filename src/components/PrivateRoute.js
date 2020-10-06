8fac2bd58128dfebaa8680ae49d961e289139019
celiaphung <celiaphung@gmail.com>
2020-10-06 14:42:00 -0400

    Attribute Private Route to docs

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,36 +1,36 @@
-import { Route, Redirect, withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
-import React from 'react';
+import { Route, Redirect, withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import React from "react";
 
+// Concept adapted from https://reactrouter.com/web/example/auth-workflow
 class PrivateRoute extends React.Component {
-    render() {
-        const { children, loggedIn, ...rest } = this.props;
-        return (
-            <Route
-                {...rest}
-                render={({ location, ...nestedProps }) =>
-                    loggedIn ? (
-                        React.cloneElement(children, { location, ...nestedProps })
-                    ) : (
-                            <Redirect
-                                to={{
-                                    pathname: "/login",
-                                    state: { from: location }
-                                }}
-                            />
-                        )
-                }
+  render() {
+    const { children, loggedIn, ...rest } = this.props;
+    return (
+      <Route
+        {...rest}
+        render={({ location, ...nestedProps }) =>
+          loggedIn ? (
+            React.cloneElement(children, { location, ...nestedProps })
+          ) : (
+            <Redirect
+              to={{
+                pathname: "/login",
+                state: { from: location },
+              }}
             />
-        );
-    }
+          )
+        }
+      />
+    );
+  }
 }
 
 function mapStateToProps({ authedUser }) {
-    const { id } = authedUser || {};
-    return {
-        loggedIn: !!id,
-    };
+  const { id } = authedUser || {};
+  return {
+    loggedIn: !!id,
+  };
 }
-
 
 export default connect(mapStateToProps)(withRouter(PrivateRoute));
